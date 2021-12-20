689bc9d82e5a0e38b102fd6cd1fa5da1b4ccee01
vbaida <vbaida1979@gmail.com>
2021-12-20 18:25:46 +0200

    gkvbnm

--- post/mainpost.js
+++ post/mainpost.js
@@ -1,52 +1,56 @@
-fetch('https://jsonplaceholder.typicode.com/posts/')
+let url = new URL(location.href)
+let idpost = url.searchParams.get('id');
+
+fetch('https://jsonplaceholder.typicode.com/posts/' + idpost)
     .then(response => response.json())
     .then(posts => {
         let div = document.createElement('div');
-    div.classList.add('container')
-            for (const post1 of posts) {
-            let divPost = document.createElement('div')
-            divPost.classList.add('post');
-            divPost.innerHTML = `
-                <h3>UserID: ${post1.userId}</h3>
-                <h4>Id: ${post1.id}</h4>
-                <h5>Title: ${post1.title}</h5>
-                <h6>Body: ${post1.body}</h6>`
-                div.append(divPost)
-                let btn = document.createElement('button')
+        div.classList.add('container')
 
-                btn.style.width = '200px'
-                btn.style.height = '50px'
-                btn.innerHTML = '<h3>Comment of post</h3>'
-                divPost.append(btn)
-                btn.onclick = () => {
-                    fetch('https://jsonplaceholder.typicode.com/comments' )
-                        .then(response => response.json())
-                        .then(comments => {
-                                let divcomment = document.createElement('div')
-                                divcomment.classList.add('container')
-                            for (const comment of comments) {
-                                if (post1.id === comment.postId) {
-                                    let divCardComments = document.createElement('div');
-                                    divCardComments.classList.add('cardComments');
-                                    divCardComments.innerHTML = `
+        let divPost = document.createElement('div')
+        divPost.classList.add('post');
+        divPost.innerHTML = `
+                <h2>UserID: ${posts.userId}</h2>
+                <h3>Id: ${posts.id}</h3>
+                <h3>Title: ${posts.title}</h3>
+                <h3>Body: ${posts.body}</h3>`
+        div.append(divPost)
+        let btn = document.createElement('button')
+
+        btn.style.width = '200px'
+        btn.style.height = '50px'
+        btn.style.background = 'blue'
+        btn.innerHTML = '<h3>Comment of post</h3>'
+        divPost.append(btn)
+        btn.onclick = () => {
+            fetch('https://jsonplaceholder.typicode.com/comments' )
+                .then(response => response.json())
+                .then(comments => {
+                        let divcomment = document.createElement('div')
+                        divcomment.classList.add('container')
+                        for (const comment of comments) {
+                            if (posts.id === comment.postId) {
+                                let divCardComments = document.createElement('div');
+                                divCardComments.classList.add('cardComments');
+                                divCardComments.innerHTML = `
                                          <h3>PostID: ${comment.postId}</h3>
                                          <h3>ID: ${comment.id}</h3>
                                         <h4>Name: ${comment.name}</h4>
-                                        <h5>Email: ${comment.email}</h5>
-                                        <h6>Body: ${comment.body}</h6>`
-                                    divPost.append(divcomment)
-                                    divcomment.append(divCardComments)
-
-                                }
+                                        <h4>Email: ${comment.email}</h4>
+                                        <h4>Body: ${comment.body}</h4>`
+                                divPost.append(divcomment)
+                                divcomment.append(divCardComments)
 
                             }
 
-                                }
+                        }
 
-                                )
+                    }
 
-                                }
-                                div.append(divPost)
-                            }
+                )
 
-            document.body.append(div)})
+
+            div.append(divPost)
+        }
+
+        document.body.append(div)})
--- user/mainuser.js
+++ user/mainuser.js
@@ -1,69 +1,74 @@
-fetch('https://jsonplaceholder.typicode.com/users/' )
+let url = new URL(location.href)
+let iduser = url.searchParams.get('id');
+
+fetch('https://jsonplaceholder.typicode.com/users/' + iduser )
     .then(response => response.json())
     .then(users => {
-        let div = document.createElement('div')
-        div.classList.add('container')
-        for (const user of users) {
+            let div = document.createElement('div')
+            div.classList.add('container')
 
-                let divuser = document.createElement('div');
-                divuser.classList.add('post');
-                div.append(divuser)
-                divuser.innerHTML = `
-                            <h2>Id: ${user.id}</h2>
-                            <h1>Name: ${user.name}</h1>
-                            <h3>Username: ${user.username} </h3>
-                            <h5>Email: ${user.email}</h5>
-                            <h5>Street: ${user.address.street}</h5>
-                            <h5>Suite: ${user.address.suite}</h5>
-                            <h5>City: ${user.address.city}</h5>
-                            <h5>Zipcode: ${user.address.zipcode}</h5>
-                            <h5>Lat: ${user.address.geo.lat}</h5>
-                            <h5>Lng: ${user.address.geo.lng}</h5>
-                            <h5>Phone: ${user.phone}</h5>
-                            <h5>Website: ${user.website}</h5>
-                            <h5>Companyname: ${user.company.name}</h5>
-                            <h5>CompanycatchPhrasee: ${user.company.catchPhrase}</h5>
-                            <h5>Companybs: ${user.company.bs}</h5>`
-                let btn = document.createElement('button')
-
-                btn.style.width = '200px'
-                btn.style.height = '50px'
-                btn.innerHTML = '<h3>Post of current user</h3>'
-                divuser.append(btn)
+            let divuser = document.createElement('div');
+            divuser.classList.add('post');
+            div.append(divuser)
+            divuser.innerHTML = `
+                            <h2>Id: ${users.id}</h2>
+                            <h1>Name: ${users.name}</h1>
+                            <h3>Username: ${users.username} </h3>
+                            <h3>Email: ${users.email}</h3>
+                            <h3>Street: ${users.address.street}</h3>
+                            <h3>Suite: ${users.address.suite}</h3>
+                            <h3>City: ${users.address.city}</h3>
+                            <h3>Zipcode: ${users.address.zipcode}</h3>
+                            <h3>Lat: ${users.address.geo.lat}</h3>
+                            <h3>Lng: ${users.address.geo.lng}</h3>
+                            <h3>Phone: ${users.phone}</h3>
+                            <h3>Website: ${users.website}</h3>
+                            <h3>Companyname: ${users.company.name}</h3>
+                            <h3>CompanycatchPhrasee: ${users.company.catchPhrase}</h3>
+                            <h3>Companybs: ${users.company.bs}</h3>`
+            let btn = document.createElement('button')
+            btn.style.width = '1500px'
+            btn.style.height = '50px'
+        btn.style.background = 'green'
+            btn.innerHTML = '<h3>Post of current user</h3>'
+            divuser.append(btn)
             btn.onclick = () => {
-                    fetch('https://jsonplaceholder.typicode.com/posts' )
-                        .then(response => response.json())
-                        .then(posts => {
+                fetch('https://jsonplaceholder.typicode.com/posts' )
+                    .then(response => response.json())
+                    .then(posts => {
                             let divcardpost = document.createElement('div')
                             divcardpost.classList.add('container')
                             for (const post of posts) {
 
-                                if (user.id === post.userId) {
+                                if (users.id === post.userId) {
                                     let divCardComments = document.createElement('div');
                                     divCardComments.classList.add('cardComments');
                                     divCardComments.innerHTML = `
-                                        <h5>Title: ${post.title}</h5>`
+                                        <h2>UserId: ${post.userId}</h2>
+                                        <h3>Id: ${post.id}</h3>
+                                        <h4>Title: ${post.title}</h4>`
 
-                                    let btn1 = document.createElement('button')
-                                    divCardComments.append(btn1)
-                                    btn1.style.width = '200px'
-                                    btn1.style.height = '50px'
-                                    btn1.innerHTML = '<a href="../post/post-details.html" class="btn">Переход по ссылке</a>'
+                                    let detailslink = document.createElement('a')
+                                    detailslink.classList.add('button')
+                                    detailslink.innerHTML = `<h2>Деталі юзера</h2>`
+                                    detailslink.href = '../post/post-details.html?id=' + post.id
 
 
-                                      divuser.append(divcardpost)
-                                        divcardpost.append(divCardComments)
-                                    divCardComments.append(btn1)
-                                                        // button.disabled = true;
-                                                    }
-                                                    }
-                                                }
-                                            )
 
-                                    }
+                                    divuser.append(divcardpost)
+                                    divcardpost.append(divCardComments)
+                                    divCardComments.append(detailslink)
+                                    // divCardComments.append(btn1)
 
+                                }
+                            }
                         }
+                    )
 
-document.body.append(div)
-}
-)
+            }
+
+
+
+            document.body.append(div)
+        }
+    )
